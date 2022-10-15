3e3bac8f619bcec8f95fab323604d0508577aaeb
rakshith <rakshithjogihalli@gmail.com>
2022-10-15 16:58:09 +0530

    base - commit

---
+++ src/main/java/org/training/library_management/LibraryManagementApplication.java
@@ -0,0 +1,20 @@
+package org.training.library_management;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
---
+++ src/test/java/org/training/library_management/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
