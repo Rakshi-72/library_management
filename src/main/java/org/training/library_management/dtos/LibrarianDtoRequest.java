c0dcd1682a10f76eb09dafe4a251e3386acf21d0
rakshith <rakshithjogihalli@gmail.com>
2022-10-26 13:22:55 +0530

    change in email validation

--- src/main/java/org/training/library_management/dtos/LibrarianDtoRequest.java
+++ src/main/java/org/training/library_management/dtos/LibrarianDtoRequest.java
@@ -22,7 +22,7 @@ public class LibrarianDtoRequest {
     private Integer age;
     private String gender;
     @Email(message = "please enter a valid email")
-    @Size(min = 8, max = 25, message = "email should be between 8-25 characters")
+    @Size(min = 8, max = 40, message = "email should be between 8-25 characters")
     private String email;
 
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "password must contain 1 capital, 1 small, 1 numeric and one special character")
